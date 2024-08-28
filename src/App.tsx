ad545e9616cf8031c9206da4498d8803b30ce1d1
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-08-28 23:24:47 +0700

    refactor: testing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { Input } from './components/ui/input';
+// import './styles/_boilerplate.scss';
+
 export function App() {
-	return <div>Hello</div>
-   }
+   return (
+      <div>
+         <Button>JASDASDASD</Button>
+         <Input placeholder="inp" label="input"></Input>
+         <Input placeholder="JASDASDASD" label="JASDASDASD" error={true}></Input>
+         <Input placeholder="disabled" label="input" disabled></Input>
+      </div>
+   );
+}
