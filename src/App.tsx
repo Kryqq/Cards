361fa74c552929ddf9e6b43d7c71880f81e8e5e6
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-08-29 21:56:53 +0700

    feat: add tabs component with radix realization

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { Input } from './components/ui/input';
-// import './styles/_boilerplate.scss';
+import './styles/_boilerplate.scss';
+import { Tab } from './components/ui/tabs/Tabs';
 
 export function App() {
    return (
@@ -9,6 +10,7 @@ export function App() {
          <Input placeholder="inp" label="input"></Input>
          <Input placeholder="JASDASDASD" label="JASDASDASD" error={true}></Input>
          <Input placeholder="disabled" label="input" disabled></Input>
+         <Tab></Tab>
       </div>
    );
 }
