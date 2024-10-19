773c924b84992e0bb72370d61206c5d1466a970c
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-10-19 20:28:43 +0700

    refactor types

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,18 +1,18 @@
 import React, { forwardRef } from 'react';
 import s from './input.module.scss';
-export type Props = {
+export type Props<T extends React.ElementType = 'input'> = {
    variant?: 'default' | 'password' | 'search';
    className?: string;
    label?: string;
    error?: boolean;
-} & React.ComponentPropsWithoutRef<'input'>;
+} & React.ComponentPropsWithoutRef<T>;
 
 export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
    const { variant = 'default', label, error, className, ...rest } = props;
 
    return (
       <div className={s.input__box}>
-         {label ? <label className={`${s.input__label}`}>{label}</label> : null}
+         {label ? <label className={s.input__label}>{label}</label> : null}
          <input
             ref={ref}
             className={` ${s.input} ${s[variant]} ${className} ${error ? s.error__input : ''} `}
