e5009b90fcef5144ff5d515630123af84eae755a
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-10-22 22:30:12 +0700

    feat add contolled-checkbox comp

---
+++ src/components/ui/controlled-checkbox/Controlled-checkbox.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { FieldValues, useController, UseControllerProps } from 'react-hook-form';
+
+import { Checkbox, СheckboxProps } from '@/components/ui';
+
+export type ControlledCheckboxProps<TFieldValues extends FieldValues> = UseControllerProps<TFieldValues> &
+   Omit<СheckboxProps, 'onChange' | 'value' | 'id'>;
+
+export const ControlledCheckbox = <TFieldValues extends FieldValues>({
+   name,
+   rules,
+   shouldUnregister,
+   control,
+   defaultValue,
+   ...checkboxProps
+}: ControlledCheckboxProps<TFieldValues>) => {
+   const {
+      field: { onChange, value },
+   } = useController({
+      name,
+      rules,
+      shouldUnregister,
+      control,
+      defaultValue,
+   });
+
+   return <Checkbox onChange={onChange} checked={value} id={name} {...checkboxProps} />;
+};
